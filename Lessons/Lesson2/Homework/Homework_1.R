6047b98c500cd841a700cea173427e1add70a450
matusrako <47779308+matusrako@users.noreply.github.com>
2019-03-05 09:15:36 +0100

    Update Homework_1.R

--- Lessons/Lesson2/Homework/Homework_1.R
+++ Lessons/Lesson2/Homework/Homework_1.R
@@ -3,21 +3,36 @@
 # __Commit__ it to your repository into `Lessons/Lesson2/Homework`.
 
 ## Code
+# Načítanie potrebných R-kových knižníc:
 library(dplyr)
 library(ggplot2)
 
+# Načítanie dát:
 dt_KPI_raw <- read.csv("./Data/lesson2_KPI.csv")
 
+# Očistenie od záporného poistného:
 dt_KPI_raw %>% 
   mutate(Premium = ifelse(Premium < 0, 0, Premium))
 
-dt_KPI_raw %>%  mutate(UWR = Premium - Expenses - Losses) %>% 
+# Hľadanie najprofitabilnejšieho portfólia:
+dt_KPI_raw %>%  
+  mutate(UWR = Premium - Expenses - Losses) %>% 
+  group_by(Unit) %>% 
+  summarize(UWR = sum(UWR, na.rm = TRUE)) %>% 
+  arrange(UWR)
+
+# Vyfiltrovanie podľa roku:
+dt_KPI_raw %>%  
+  mutate(UWR = Premium - Expenses - Losses) %>% 
+  filter(Unit == "Unit7") %>% 
   group_by(Year) %>% 
   summarize(UWR = sum(UWR, na.rm = TRUE)) %>% 
   arrange(UWR)
 
+# Grafické znázornenie:
 dt_KPI_raw %>% 
   mutate(UWR = Premium - Expenses - Losses) %>% 
+  filter(Unit == "Unit7") %>% 
   group_by(Year) %>% 
   summarize(UWR = sum(UWR, na.rm = TRUE)) %>% 
   ggplot(aes(x = reorder(Year, UWR), y = UWR)) + 
@@ -27,8 +42,10 @@ dt_KPI_raw %>%
 
 
 # Your Explanation about analysis:
-1) Očistil som dáta.
-2) Zoskupil som ich podľa rokov.
-3) Porovnal som ich podľa Underwriting Result a graficky znázornil.
-Výsledok: Najhorší rok vyšiel 2015.
-Dôvod: Rok 2015 má najmenšiu hodnotu UWR.
+1) Očistil som dáta od záporného poistného.
+2) Vyhľadal som najprofitabilnejšie portfólio (t. j. s najvyššou hodnotou Underwriting Result) -> Unit7.
+3) Vyfiltroval som Underwriting Result pre Unit7.
+4) Zoskupil som to podľa rokov.
+5) Porovnal som ich Underwriting Result a graficky znázornil.
+Výsledok: Najhorší rok pre najprofitabilnejšie portfólio Unit7 vyšiel 2014.
+Dôvod: Rok 2014 má najmenšiu hodnotu Underwriting Result.
